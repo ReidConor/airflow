3e68d6bac9d1df9d565d25bb7567d563d92c7713
Conor Reid <conor.reid@ucdconnect.ie>
2018-05-20 20:27:19 +0100

    dags folder

---
+++ dags/cgcp.py
@@ -0,0 +1,30 @@
+import os
+from datetime import datetime
+from airflow import DAG
+from airflow.configuration import conf
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+
+buildFileConf = conf['cgcp']['buildFile']
+print(buildFileConf)
+buildFile="/Users/Conor/Google\ Drive/MSc/_dissertation/cgcp/build.sh "
+print(buildFile)
+
+dag = DAG(
+    'cgcp',
+    description='DAG for dissertation scripts',
+    schedule_interval=None,
+    start_date=datetime(2018, 5, 20),
+    catchup=False
+)
+
+dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
+
+build_proj = BashOperator(
+    task_id= 'build',
+    bash_command=buildFileConf,
+    dag=dag
+)
+
+dummy_operator >> build_proj
