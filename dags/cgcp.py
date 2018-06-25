7eaceffb9a20ae39e2eea82e899aa989c7c688af
Conor Reid <conor.reid@ucdconnect.ie>
2018-06-25 19:50:30 +0100

    more tasks

--- dags/cgcp.py
+++ dags/cgcp.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.configuration import conf
 from airflow.operators.dummy_operator import DummyOperator
@@ -14,6 +14,13 @@ completeCasesFile = os.environ["AIRFLOW_CGCP_FILE_LOCATION_COMPLETE_CASES"]
 imputeFile = os.environ["AIRFLOW_CGCP_FILE_LOCATION_IMPUTE"]
 imputeScaleFile = os.environ["AIRFLOW_CGCP_FILE_LOCATION_IMPUTE_SCALE"]
 covariateCorrelationFile = os.environ["AIRFLOW_CGCP_FILE_LOCATION_COVARIATE_CORRELATION"]
+extraVarsFile = os.environ["AIRFLOW_CGCP_FILE_LOCATION_EXTRA_VARS"]
+causalEstimationFile = os.environ["AIRFLOW_CGCP_FILE_LOCATION_CAUSAL_EST"]
+
+default_args = {
+    'retries': 5,
+    'retry_delay': timedelta(minutes=1)
+}
 
 dag = DAG(
     'cgcp',
@@ -41,6 +48,12 @@ dbManipulations = BashOperator(
     dag=dag
 )
 
+extraVars = BashOperator(
+    task_id= 'extraVars',
+    bash_command=extraVarsFile,
+    dag=dag
+)
+
 completeCases = BashOperator(
     task_id= 'completeCases',
     bash_command=completeCasesFile,
@@ -65,4 +78,10 @@ covariateCorrelation = BashOperator(
     dag=dag
 )
 
-build >> load >> dbManipulations >> completeCases >> impute >> impute_scale >> covariateCorrelation
+causalEstimation = BashOperator(
+    task_id= 'causalEstimation',
+    bash_command=causalEstimationFile,
+    dag=dag
+)
+
+build >> load >> dbManipulations >> extraVars >> completeCases >> impute >> impute_scale >> covariateCorrelation >> causalEstimation
